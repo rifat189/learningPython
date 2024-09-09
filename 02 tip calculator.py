1df9ca08acc02c8428f54f5eca2b66b2b9c5109f
rifat189 <115808634+rifat189@users.noreply.github.com>
2024-09-09 20:28:20 +0600

    Update 02 tip calculator.py

--- 02 tip calculator.py
+++ 02 tip calculator.py
@@ -6,3 +6,8 @@ total = a + tip
 c = int(input("How many people to split the bill?\n"))
 per = (a+b)/c
 print(f"Each person should pay: ${round(per, 2)}")
+
+# for formatting to 2 decimal points always (ex: 32.5 to 32.50)
+per = "{:.2f}".format(round(per, 2))
+print(f"Each person should pay (formatted): ${per}")
+

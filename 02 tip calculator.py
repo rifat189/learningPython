24756ec2993b45551592e4fb254b45983ff09942
rifat189 <115808634+rifat189@users.noreply.github.com>
2024-09-09 20:20:09 +0600

    Update 02 tip calculator.py

--- 02 tip calculator.py
+++ 02 tip calculator.py
@@ -1,7 +1,7 @@
 print("Welcome to the tip calculator!")
 a = float(input("What was the total bill?\n$"))
 b = int(input("How much tip would you like to give?\n$"))
-tip = float(b/100)
+tip = a * (b / 100)
 total = a + tip
 c = int(input("How many people to split the bill?\n"))
 per = (a+b)/c
